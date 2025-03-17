4dd8536b503d644139053a7f47752da4cfc1f970
rubensmanhaesb <115162453+rubensmanhaesb@users.noreply.github.com>
2025-03-17 13:01:30 -0300

    refactor: reorganizando o código e namesspaces

---
+++ RMB.Responses/Logs/Extensions/Logs/EventLogManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RMB.Responses.Logs.Extensions.Logs
+{
+    /// <summary>
+    /// Provides utilities for managing Windows Event Viewer logs.
+    /// </summary>
+    public static class EventLogManager
+    {
+        /// <summary>
+        /// Ensures the specified Event Source exists in the Windows Event Viewer.
+        /// </summary>
+        /// <param name="sourceName">The name of the Event Source to be created if it does not exist.</param>
+        /// <param name="logName">The name of the Event Log where events will be recorded.</param>
+        public static void EnsureEventSourceExists(string logName)
+        {
+            if (!EventLog.SourceExists(logName))
+            {
+                try
+                {
+                    var sourceData = new EventSourceCreationData(logName, logName);
+                    EventLog.CreateEventSource(sourceData);
+
+                    Console.WriteLine($"Fonte de log '{sourceData}' criada no log '{logName}'.");
+                }
+                catch (System.Security.SecurityException)
+                {
+                    Console.WriteLine($"Sem permissão para criar a fonte de log '{logName}'. Execute como Administrador.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro inesperado ao criar a fonte de log: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ RMB.Responses/Logs/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using RMB.Responses.Entities;
+using System;
+using System.Threading.Tasks;
+
+
+namespace RMB.Responses.Middlewares.Logs
+{
+
+    public class CorrelationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorrelationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, CorrelationContext correlationContext)
+        {
+            // Se já vier um CorrelationId no cabeçalho, usa ele, senão gera um novo
+            if (context.Request.Headers.TryGetValue("X-Correlation-ID", out var correlationId))
+            {
+                correlationContext.CorrelationId = correlationId;
+            }
+            else
+            {
+                correlationContext.CorrelationId = Guid.NewGuid().ToString();
+                context.Response.Headers.Add("X-Correlation-ID", correlationContext.CorrelationId);
+            }
+
+            await _next(context);
+        }
+    }
+
+}
