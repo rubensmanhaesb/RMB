882c9b43fd216eb235d7f1422e59a8e65b1e358d
rubensmanhaesb <115162453+rubensmanhaesb@users.noreply.github.com>
2025-03-18 01:51:23 -0300

    chore: adiciona arquivo fora da demanda

---
+++ RMB.Abstractions/UseCases/Logs/ICorrelationIdProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMB.Abstractions.UseCases.Logs
+{
+    /// <summary>
+    /// Interface for managing the Correlation ID.
+    /// </summary>
+    public interface ICorrelationIdProvider
+    {
+        /// <summary>
+        /// Retrieves or generates a Correlation ID for the current request.
+        /// </summary>
+        string GetCorrelationId(HttpContext context);
+
+        /// <summary>
+        /// Ensures the Correlation ID is included in the response headers.
+        /// </summary>
+        void AddCorrelationIdToResponse(HttpContext context);
+    }
+}
