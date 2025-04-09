b4e5c26f17b9a31235a81e35e179c233aa626f1c
rubensmanhaesb <115162453+rubensmanhaesb@users.noreply.github.com>
2025-04-09 09:53:40 -0300

    refact: Adicionando mais regras de validação para envio de email

--- RMB.Infrastructure/RMB.Infrastructure.Messages/Middlewares/Validations/EmailConfirmationMessageValidaton.cs
+++ RMB.Infrastructure/RMB.Infrastructure.Messages/Middlewares/Validations/EmailConfirmationMessageValidaton.cs
@@ -10,9 +10,17 @@ namespace RMB.Infrastructure.Messages.Middlewares.Validations
         {
             RuleFor(x => x.ToEmail)
                 .NotEmpty().WithMessage("O campo 'ToEmail' é obrigatório.")
-                .EmailAddress().WithMessage("O campo 'ToEmail' precisa ser um e-mail válido.");
+                .EmailAddress().WithMessage("O campo 'ToEmail' precisa ser um e-mail válido.")
+                .MaximumLength(254).WithMessage("O e-mail não pode ultrapassar 254 caracteres.")
+                .Must(email => !string.IsNullOrWhiteSpace(email)).WithMessage("O campo 'ToEmail' não pode conter apenas espaços em branco.")
+                .Must(email => !email.Any(char.IsControl)).WithMessage("O e-mail não pode conter caracteres de controle.")
+                .Matches(@"^[^<>]*$").WithMessage("O e-mail não pode conter tags HTML.")
+                .Must(email => !email.Contains('<') && !email.Contains('>')).WithMessage("O e-mail não pode conter caracteres HTML como '<' ou '>'.");
+
 
             RuleFor(x => x.ConfirmationLink)
+                .Must(link => Uri.IsWellFormedUriString(link, UriKind.Absolute))
+                .WithMessage("O link de confirmação precisa ser uma URL válida.")
                 .NotEmpty().WithMessage("O campo 'ConfirmationLink' é obrigatório.");
         }
     }
